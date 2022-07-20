f366f769c356d639456939a092eee6c5cd4171ba
Tiramis1 <isiktiramis@gmail.com>
2022-07-20 17:04:40 +0200

    Updated the date information

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 function displayTemperature(response){
     console.log(response.data);
 
+function dateFormat(timestamp){
+    let date= new Date(timestamp);
+    let hours=date.getHours();
+    if (hours<10){
+        hours=`0${hours}`;
+    }
+    let minutes=date.getMinutes();
+    if (minutes<10){
+        minutes=`0${minutes}`;
+    }
+    let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day=days[date.getDay()];
+    return `${day} ${hours}:${minutes};
+}
+
     let cityElement= document.querySelector("#city");
     cityElement.innerHTML=response.data.name;
 
@@ -17,6 +32,9 @@ function displayTemperature(response){
 
     let windElement=document.querySelector("#wind");
     windElement.innerHTML=Math.round(response.data.wind.speed);
+
+    let dateElement=document.querySelector("#date");
+    dateElement.innerHTML= dateFormat(response.data.dt*1000);
 }
 
 let apiKey= "0951c90b2bac386d03348c6017a913c9";
